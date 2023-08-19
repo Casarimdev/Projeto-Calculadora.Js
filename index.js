ebd0a8c27367e0fa2c92fdc3ae58d5b5fb85f7be
Casarimdev <casarim.dev@gmail.com>
2023-08-19 20:15:31 -0300

    ..

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const main = document.querySelector('main')
 const root = document.querySelector(':root')
 const input = document.getElementById('input')
 const resultInput = document.getElementById('result')
-const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
+const allowedKeys = ["(", ")", "/", "x", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
 
 document.getElementById("clear").addEventListener('click',function(){
   input.value = ""
